eb6aa79dbdd8775e257e142d203770fa10202ebf
enan789 <enan7890@gmail.com>
2018-08-20 02:38:46 -0400

    game form and validation complete

---
+++ Mastermind/game/src/components/CodeForm.js
@@ -0,0 +1,170 @@
+import React from 'react';
+
+class CodeForm extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      d1: '',
+      d2: '',
+      d3: '',
+      d4: '',
+      formErrors: {d1: '', d2: '', d3: '', d4: ''},
+      d1Valid: false,
+      d2Valid: false,
+      d3Valid: false,
+      d4Valid: false,
+      formValid: false
+    }
+  }
+
+  handleUserInput (e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({[name]: value},
+                () => { this.validateField(name, value) });
+  }
+
+  validateField(fieldName, value) {
+    let fieldValidationErrors = this.state.formErrors;
+    let d1Valid = this.state.d1Valid;
+    let d2Valid = this.state.d2Valid;
+    let d3Valid = this.state.d3Valid;
+    let d4Valid = this.state.d4Valid;
+
+    let length = false;
+    if(value){
+      length = value.length == 1;
+    }
+    let number = !isNaN(value);
+
+    let message = ''
+
+    if(!number){
+      message = ' is not a number';
+    } else {
+      message = length ? '' : ' is not a digit'
+    }
+
+    switch(fieldName) {
+      case 'd1':
+        d1Valid = number && length;
+        fieldValidationErrors.d1 = message;
+        break;
+      case 'd2':
+        d2Valid = number && length;
+        fieldValidationErrors.d2 = message;
+        break;
+      case 'd3':
+        d3Valid = number && length;
+        fieldValidationErrors.d3 = message;
+        break;
+      case 'd4':
+        d4Valid = number && length;
+        fieldValidationErrors.d4 = message;
+        break;
+      default:
+        break;
+      }
+      this.setState({formErrors: fieldValidationErrors,
+                      d1Valid: d1Valid,
+                      d2Valid: d2Valid,
+                      d3Valid: d3Valid,
+                      d4Valid: d4Valid,
+                    }, this.validateForm);
+  }
+
+  validateForm() {
+    console.log(this.state);
+    this.setState({formValid: this.state.d1Valid && this.state.d2Valid
+      && this.state.d3Valid && this.state.d4Valid });
+    console.log(this.state);
+  }
+
+  errorClass(error) {
+    return(error.length === 0 ? '' : 'has-error');
+  }
+
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state);
+  }
+
+
+
+  render() {
+    var errors = [];
+
+    for(var key in this.state.formErrors) {
+      if(this.state.formErrors.hasOwnProperty(key)) {
+        if(this.state.formErrors[key]){
+          errors.push('Digit ' + key[1] + ' ' + this.state.formErrors[key]);
+        }
+      }
+    }
+
+    var htmlErrors = errors.map(function(error) {
+      return (
+        <div>
+          {error}
+        </div>
+      );
+    });
+
+    var errorBox = (
+      <div className='alert alert-danger'>
+        {htmlErrors}
+      </div>);
+
+    console.log(errors.length==0)
+
+    return (
+      <form>
+        <div class="columns">
+          <div className={`form-group ${this.errorClass(this.state.formErrors.d1)}`}>
+            <input
+              name="d1"
+              value={this.state.d1}
+              class="form-control"
+              type="text"
+              onChange = {(event) => this.handleUserInput(event)}
+            />
+          </div>
+          <div className={`form-group ${this.errorClass(this.state.formErrors.d2)}`}>
+            <input
+              name="d2"
+              value={this.state.d2}
+              class="form-control"
+              type="text"
+              onChange = {(event) => this.handleUserInput(event)}
+            />
+          </div>
+          <div className={`form-group ${this.errorClass(this.state.formErrors.d3)}`}>
+            <input
+              name="d3"
+              value={this.state.d3}
+              class="form-control"
+              type="text"
+              onChange = {(event) => this.handleUserInput(event)}
+            />
+          </div>
+          <div className={'form-group has-error'}>
+            <input
+              name="d4"
+              value={this.state.d4}
+              class="form-control"
+              type="text"
+              onChange = {(event) => this.handleUserInput(event)}
+            />
+          </div>
+        </div>
+        {(this.state.formValid || errors == 0) ? '' : errorBox}
+        <div class="container">
+          <button onClick={e => this.onSubmit(e)} class="btn btn-primary"
+           disabled={!this.state.formValid}>Enter Code</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default CodeForm;
--- Mastermind/game/src/components/GamePlay.js
+++ Mastermind/game/src/components/GamePlay.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import CodeForm from './CodeForm';
 
 class GamePlay extends Component {
   constructor(){
@@ -39,12 +40,32 @@ class GamePlay extends Component {
     }
 
     if( this.right == 4){
-      win();
+      this.win();
     } else if(turn == 0) {
-      lose();
+      this.lose();
     } else {
       turn -= 1;
     }
+  }
+
+  onSubmit = fields => {
+    console.log('Recieved: ', fields);
+    var inputs = []
+    inputs.push(fields.d1)
+    inputs.push(fields.d2)
+    inputs.push(fields.d3)
+    inputs.push(fields.d4)
+    console.log(inputs)
+
+
+  }
+
+  win(){
+    console.log('win');
+  }
+
+  lose(){
+    console.log('win');
   }
 
   render() {
@@ -55,22 +76,7 @@ class GamePlay extends Component {
           <h3 class="text-center">Digits do not repeat</h3>
         </div>
         <br/>
-
-        <form>
-          <div class="columns">
-            <input class="container" type="text"/>
-            <br/>
-            <input class="container" type="text"/>
-            <br/>
-            <input class="container" type="text"/>
-            <br/>
-            <input class="container" type="text"/>
-          </div>
-          <div class="container">
-            <button class="btn btn-primary">Enter Code</button>
-          </div>
-        </form>
-
+        <CodeForm onSubmit={fields => this.onSubmit(fields)}/>
       </div>
     );
   }
